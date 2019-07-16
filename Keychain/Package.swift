b1983b74a61c73e557654cbe66f88308d8b5fae6
Ted Conley <ted.conley@gmail.com>
2019-07-16 10:41:50 -0400

    Updated project file, added Package.swift

---
+++ Keychain/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "SwiftKeychain",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
+    products: [
+        .library(
+            name: "Keychain",
+            targets: ["Keychain"])
+    ],
+    targets: [
+        .target(
+            name: "Keychain",
+            path: "Keychain")
+    ],
+    swiftLanguageVersions: [.v5]
+)
